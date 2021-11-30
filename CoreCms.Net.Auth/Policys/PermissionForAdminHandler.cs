02a84b163d937730e8c4341ea4d4fe33102c94f3
大灰灰 <jianweie@163.com>
2021-11-30 15:11:35 +0000

    !176 【修复】修复uview1.8.4格式化富文本，图片之间有白色间隔，导致商品多张分割图不衔接的问题。#I4KE6N【优化】优化权限管理未认证导致标准表单提交无验证问题。感谢【匿名者QQ:674850024】发现此问题。 * ## 1.2.3（2021-11-30） * ## 1.2.1（2021-11-29） * 【优化】调整本地存储域名规则，防止出现前后端不统一问题。#I4K56N

--- CoreCms.Net.Auth/Policys/PermissionForAdminHandler.cs
+++ CoreCms.Net.Auth/Policys/PermissionForAdminHandler.cs
@@ -186,8 +186,13 @@ namespace CoreCms.Net.Auth.Policys
                         return;
                     }
                 }
+                else
+                {
+                    context.Fail();
+                    return;
+                }
                 //判断没有登录时，是否访问登录的url,并且是Post请求，并且是form表单提交类型，否则为失败
-                if (!questUrl.Equals(requirement.LoginPath.ToLower(), StringComparison.Ordinal) && (!httpContext.Request.Method.Equals("POST") || !httpContext.Request.HasFormContentType))
+                if (!questUrl.Equals(requirement.LoginPath.ToLower(), StringComparison.Ordinal) && (!httpContext.Request.Method.Equals("POST") || !httpContext.Request.HasJsonContentType()))
                 {
                     context.Fail();
                     return;
