74975812b01592f77c4073afff85613d0e4a914b
capad1946 <wizard1946@163.com>
2023-05-07 15:06:15 +0000

    !212 删除BaseRepository中的一些冗余代码 * 删除一些冗余代码

---
+++ CoreCms.Net.Repository/SqlSugarExtensions.cs
@@ -0,0 +1,17 @@
+﻿using SqlSugar;
+
+namespace CoreCms.Net.Repository
+{
+	internal static class SqlSugarExtensions
+	{
+		internal static ISugarQueryable<T> WithNoLockOrNot<T>(this ISugarQueryable<T> query,bool @lock = false)
+		{
+			if (@lock)
+			{
+				query = query.With(SqlWith.NoLock);
+			}
+
+			return query;
+		}
+	}
+}
