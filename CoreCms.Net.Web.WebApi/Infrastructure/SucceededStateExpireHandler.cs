6cccc3c4017bc98ac17aebed95d6230fa740e366
jianweie code <jianweie@live.com>
2024-06-19 09:47:11 +0800

    增加hangfire日志增长限制，移除hangfire默认账号密码，由用户自行设置。

---
+++ CoreCms.Net.Web.WebApi/Infrastructure/SucceededStateExpireHandler.cs
@@ -0,0 +1,51 @@
+﻿using Hangfire.States;
+using Hangfire.Storage;
+using System;
+
+namespace CoreCms.Net.Web.WebApi.Infrastructure
+{
+    /// <summary>
+    /// 已完成的job设置过期，防止数据无限增长
+    /// </summary>
+    public class SucceededStateExpireHandler : IStateHandler
+    {
+        /// <summary>
+        /// 数据过期时间
+        /// </summary>
+        public TimeSpan JobExpirationTimeout;
+
+        /// <summary>
+        /// 完成的项目过期状态处理
+        /// </summary>
+        /// <param name="jobExpirationTimeout"></param>
+        public SucceededStateExpireHandler(int jobExpirationTimeout)
+        {
+            JobExpirationTimeout = TimeSpan.FromMinutes(jobExpirationTimeout);
+        }
+
+        /// <summary>
+        /// 状态名称
+        /// </summary>
+        public string StateName => SucceededState.StateName;
+
+        /// <summary>
+        /// 应用状态
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="transaction"></param>
+        public void Apply(ApplyStateContext context, IWriteOnlyTransaction transaction)
+        {
+            context.JobExpirationTimeout = JobExpirationTimeout;
+        }
+
+        /// <summary>
+        /// 不应用状态
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="transaction"></param>
+        public void Unapply(ApplyStateContext context, IWriteOnlyTransaction transaction)
+        {
+        }
+    }
+
+}
